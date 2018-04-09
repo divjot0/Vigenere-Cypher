de706c90d03a9381f3fcd10311e27633339d37a0
Piyush Pilaniya <piyushpilaniya98@gmail.com>
2018-04-09 12:19:11 +0530

    Update operator.c

--- operator.c
+++ operator.c
@@ -3,8 +3,7 @@
 
 int main(){
     //we can contribute here    
-    //welcome to divjot's world
-    
+    //welcome to divjot's world    
           //You can find crypto material here
 return 0;
 }
